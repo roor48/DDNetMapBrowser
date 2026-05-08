1d58bb722160cdaa7590583a5b46695d9103a4dd
roor48 <initial_commit@naver.com>
2026-05-08 18:32:27 +0900

    fix: remove base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DDNetMapBrowser/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
